7ad644ef7221ce33db4f822fcf2972d8ae148119
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-07-19 14:04:48 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
 class Ship
+
+	@@all = []
+
+	attr_accessor :name, :type, :booty 
+
+	def initialize(params)
+		@name = params[:name]
+		@type = params[:type] 
+		@booty = params[:booty]
+		@@all << self
+	end 
+
+	def self.all 
+		@@all
+	end 
+
+	def self.clear 
+		@@all.clear 
+	end 
+
+
 end
